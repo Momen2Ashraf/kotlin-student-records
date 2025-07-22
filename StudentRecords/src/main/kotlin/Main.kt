356a15b091965321d9f97e72b41846492c24d882
momenmust <200013269@must.edu.eg>
2025-07-22 05:49:12 +0300

    implement Student Records app

--- StudentRecords/src/main/kotlin/Main.kt
+++ StudentRecords/src/main/kotlin/Main.kt
@@ -3,14 +3,25 @@ package org.example
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val students = arrayOf("Ahmed","Momen","Hassan","Mohamed")
+    val students = arrayOf("Ahmed", "Momen", "Hassan", "Mohamed", "Hamza", "Alaa")
     val studentGrades = mutableMapOf(
         "${students[0]}" to 50,
         "${students[1]}" to 100,
         "${students[2]}" to 60,
-        "${students[3]}" to 70
+        "${students[3]}" to 70,
+        "${students[4]}" to 10,
+        "${students[5]}" to 5,
     )
-    studentGrades["Hamza"] = 60
-    val graduatedStudents = setOf(studentGrades.filter { it.value >= 60 })
-    println(graduatedStudents)
+    studentGrades["Moaz"] = 80
+//    GenrateReport(studentGrades)
+    val studentGrades1 = mapOf<String,Int>()
+    GenrateReport(studentGrades1)
+}
+
+fun GenrateReport(studentGrades: Map<String, Int>){
+    if(studentGrades.isNotEmpty()) {
+        val filtered = studentGrades.filter { it.value >= 60 }
+        filtered.forEach { println("${it.key}:${it.value}") }
+    }else
+        println("please fill the map with elements !")
 }
